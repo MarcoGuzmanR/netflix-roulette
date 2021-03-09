d56ff1b74b228b149de799d9f55f6dd2f2b530b2
Marco Guzman <marco_guzman@epam.com>
2021-03-09 12:29:34 -0600

    Added styling to the MovieDetails component

--- src/components/movieDetails/details.js
+++ src/components/movieDetails/details.js
@@ -7,7 +7,29 @@ function MovieDetails() {
 
   return (
     <div className="details-main-container">
-      <p onClick={() => setSearchPage(true)}>{movieDetails.id}</p>
+      <div className="search-back-container">
+        <button type="button" className="btn-search-back" onClick={() => setSearchPage(true)}>
+          BACK TO SEARCH
+        </button>
+      </div>
+
+      <div className="details-container">
+        <div className="poster-container">
+          <img src={movieDetails.poster_path} height="461" width="337" />
+        </div>
+        <div className="info-container">
+          <div className="title-container">
+            <span className="title-details">{movieDetails.title}</span>
+            <span className="average-details">{movieDetails.vote_average}</span>
+            <p className="genres">{movieDetails.genres.join(', ')}</p>
+          </div>
+          <div className="date-container">
+            <span>{new Date(movieDetails.release_date).getFullYear()}</span>
+            <span>{movieDetails.runtime} min</span>
+          </div>
+          <p>{movieDetails.overview}</p>
+        </div>
+      </div>
     </div>
   );
 };
