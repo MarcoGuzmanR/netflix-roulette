eb4ee6ba551802b8b1cd9ca8dff7dd4431d62b9a
Marco Guzman <marco_guzman@epam.com>
2021-04-12 23:19:56 -0500

    Added snapshot test for NotFound component

--- src/__tests__/notFound.test.js
+++ src/__tests__/notFound.test.js
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom';
 import * as React from 'react'
-import {render, screen} from '@testing-library/react';
+import { render, screen, cleanup } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import { createMemoryHistory } from 'history';
 import { Router } from 'react-router-dom'
 import NotFound from '../components/movieNotFound/notFound';
@@ -8,6 +9,11 @@ import NotFound from '../components/movieNotFound/notFound';
 test('Shows the not found page message for a wrong movie page', () => {
   const history = createMemoryHistory()
   history.push('/movie-not-found');
+
+  const tree = renderer.create(
+    <Router history={history}>
+      <NotFound />
+    </Router>).toJSON();
 
   render(
     <Router history={history}>
@@ -18,4 +24,6 @@ test('Shows the not found page message for a wrong movie page', () => {
   expect(screen.getByText(/page not found/i)).toBeInTheDocument();
   expect(screen.getByText(/404/i)).toBeInTheDocument();
   expect(screen.getByText(/go back to home/i)).toBeInTheDocument();
+
+  expect(tree).toMatchSnapshot();
 });
